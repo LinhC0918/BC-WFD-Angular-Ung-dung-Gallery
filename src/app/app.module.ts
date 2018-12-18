d58436b14199137a1b16d81fdea0a8d69583c4f0
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2018-12-18 09:25:26 +0700

    add new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageGalleryModule } from './image-gallery/image-gallery.module';
 import { GalleryConfig } from './image-gallery/token';
+import { ImgSliderModule } from './img-slider/img-slider.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ImageGalleryModule
+        ImageGalleryModule,
+        ImgSliderModule
+        
     ],
     providers: [{ provide: GalleryConfig, useValue: 2 }],
     bootstrap: [AppComponent]
